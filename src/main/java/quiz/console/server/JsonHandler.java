62d65d1cb427f1d8261f83882cf15285bb69a46d
Nikita777-coder <61533809+Nikita777-coder@users.noreply.github.com>
2023-07-11 15:28:40 +0300

    add java mapping with gson

--- src/main/java/quiz/console/server/JsonHandler.java
+++ src/main/java/quiz/console/server/JsonHandler.java
@@ -1,53 +1,23 @@
 package quiz.console.server;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
+import java.lang.reflect.Type;
 import java.util.List;
 
 @Slf4j
 class JsonHandler {
     private final Gson GSON = new Gson();
-    private AnswerAndQuestion getAnswerAndQuestion(JsonElement jsonElement, int i) {
-        JsonObject jsonObject = GSON.fromJson(jsonElement, JsonObject.class);
-
-        if (jsonObject == null) {
-            throw new IllegalArgumentException(String.format("JsonElement[%d] is null!", i));
-        }
-
-        int id;
-
-        if (jsonObject.get("id") == null) {
-            throw new IllegalArgumentException("json str ans hasn't id");
-        }
-
-        id = jsonObject.get("id").getAsInt();
-
-        if (jsonObject.get("question") == null) {
-            throw new IllegalArgumentException(String.format("Json object with id = %d hasn't question ", id));
-        }
-
-        String question = jsonObject.get("question").getAsString();
-
-        if (jsonObject.get("answer") == null) {
-            throw new IllegalArgumentException(String.format("Json object with id = %d hasn't answer ", id));
-        }
-
-        String answer = jsonObject.get("answer").getAsString();
-
-        return new AnswerAndQuestion(question, answer);
-    }
 
     public void parseAnswersAndQuestions(@NonNull String output, List<AnswerAndQuestion> questionsAndAnswers) {
-        JsonArray jsonArray = GSON.fromJson(output, JsonArray.class);
+        Type type = new TypeToken<List<AnswerAndQuestion>>(){}.getType();
+        List<AnswerAndQuestion> arr = GSON.fromJson(output, type);
 
-        for (int i = 0; i < jsonArray.size(); ++i) {
+        for (AnswerAndQuestion answerAndQuestion : arr) {
             try {
-                AnswerAndQuestion answerAndQuestion = getAnswerAndQuestion(jsonArray.get(i), i);
                 questionsAndAnswers.add(answerAndQuestion);
                 log.info("ok getting new AnswerAndQuestion");
             } catch (IllegalArgumentException ex) {
